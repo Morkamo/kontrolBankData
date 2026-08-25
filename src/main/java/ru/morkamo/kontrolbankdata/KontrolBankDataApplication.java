19240b9cf373c9591970b3c63d300114c6da720b
Kirill Drozdov <131317714+Morkamo@users.noreply.github.com>
2026-08-25 10:33:26 +0900

    Initial commit

---
+++ src/main/java/ru/morkamo/kontrolbankdata/KontrolBankDataApplication.java
@@ -0,0 +1,13 @@
+package ru.morkamo.kontrolbankdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KontrolBankDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KontrolBankDataApplication.class, args);
+    }
+
+}
